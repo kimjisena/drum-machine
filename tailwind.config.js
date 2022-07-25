8f7a7be0b76be050f53a912c52354adf481fe50b
jisena.btc <pjohmaths@gmail.com>
2022-07-25 22:33:17 +0300

    add react-icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,18 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      'black-one': '#182024',
+      'black-two': '#2A2D3A',
+      'white': '#CDDFEC',
+      'red': '#F72035',
+      'yellow': '#ECFF09'
+    },
+    fontFamily: {
+      'display-one': ['"Ruslan Display"', 'cursive'],
+      'display-two': ['"Major Mono Display"', 'monospace'],
+      'drum-logo': ['"Big Shoulders Stencil Display"', 'cursive']
+    },
     extend: {},
   },
   plugins: [],
