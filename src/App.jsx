a62ebd021fb2c9155ee2e48bfde50ec906d9821f
jisena.btc <pjohmaths@gmail.com>
2022-07-26 00:28:59 +0300

    implement mini-pad

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className={`app w-full h-[100vh] flex flex-col justify-center items-center`}>
+      <div id="drum-machine" className={`relative flex flex-col items-center w-1/4 h-1/2 text-xl border-2 bg-black-one`}>
+        <div id="display" className={`absolute bottom-full w-3/5 h-1/3 bg-black-one`}>Display</div>
+
+        <div className={`pad absolute -left-8 -top-8 flex justify-center items-center bg-black-one rounded-full w-20 h-20`}>
+          <div className={`flex justify-center items-center w-[90%] h-[90%] bg-white rounded-full`}>
+            <div id="hi-hat" className={`drum-pad flex justify-center items-center w-[70%] h-[70%] bg-black-one rounded-full text-white`}>
+              Q
+              <audio id="Q" src="" className={`clip`}></audio>
+            </div>
+          </div>
+        </div>
+
+        <div className={`pad absolute -right-8 -top-8 flex justify-center items-center bg-black-one rounded-full w-20 h-20`}>
+          <div className={`flex justify-center items-center w-[90%] h-[90%] bg-white rounded-full`}>
+            <div className={`w-[70%] h-[70%] bg-black-one rounded-full`}></div>
+          </div>
+        </div>
+
+
+        <div id="hi-hat" className={`drum-pad`}>
+          Q
+          <audio id="Q" src="" className={`clip`}></audio>
+        </div>
+
+        <div id="crash" className={`drum-pad`}>
+          W
+          <audio id="W" src="" className={`clip`}></audio>
+        </div>
+        <div id="hi-hat2" className={`drum-pad`}>
+          E
+          <audio id="E" src="" className={`clip`}></audio>
+        </div>
+        <div id="tom1" className={`drum-pad`}>
+          A
+          <audio id="A" src="" className={`clip`}></audio>
+        </div>
+        <div id="ride" className={`drum-pad`}>
+          S
+          <audio id="S" src="" className={`clip`}></audio>
+        </div>
+        <div id="kick" className={`drum-pad`}>
+          D
+          <audio id="D" src="" className={`clip`}></audio>
+        </div>
+        <div id="snare" className={`drum-pad`}>
+          Z
+          <audio id="Z" src="" className={`clip`}></audio>
+        </div>
+        <div id="tom3" className={`drum-pad`}>
+          X
+          <audio id="X" src="" className={`clip`}></audio>
+        </div>
+        <div id="tom2" className={`drum-pad`}>
+          C
+          <audio id="C" src="" className={`clip`}></audio>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MiniPad.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function MiniPad() {
+  return (
+    <>
+        <div className={`pad absolute -left-8 -top-8 flex justify-center items-center bg-black-one rounded-full w-20 h-20`}>
+          <div className={`flex justify-center items-center w-[90%] h-[90%] bg-white rounded-full`}>
+            <div id="hi-hat" className={`drum-pad flex justify-center items-center w-[70%] h-[70%] bg-black-one rounded-full text-white`}>
+              Q
+              <audio id="Q" src="" className={`clip`}></audio>
+            </div>
+          </div>
+        </div>
+    </>
+  );
+}
+
+export default MiniPad;
