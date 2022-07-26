c3f214b156aed065cd8500d9eba377224792bab8
jisena.btc <pjohmaths@gmail.com>
2022-07-26 03:08:03 +0300

    drum machine ui complete

--- src/components/MidPad.jsx
+++ src/components/MidPad.jsx
@@ -5,7 +5,7 @@ function MidPad({pos, name, id}) {
     <>
         <div className={`pad absolute ${pos} flex justify-center items-center bg-black-one rounded-full w-24 h-24`}>
           <div className={`flex justify-center items-center w-[90%] h-[90%] bg-white rounded-full`}>
-            <div id={name} className={`drum-pad flex justify-center items-center w-[70%] h-[70%] bg-black-one rounded-full text-white`}>
+            <div id={name} className={`drum-pad flex justify-center items-center w-[70%] h-[70%] bg-black-one rounded-full text-white text-3xl font-bold`}>
               {id}
               <audio id={id} src="" className={`clip`}></audio>
             </div>
