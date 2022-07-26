536b1b3c9ede948eace936498c891cdafb9644bd
jisena.btc <pjohmaths@gmail.com>
2022-07-26 09:08:06 +0300

    add pad activity state indicator

--- src/components/MaxPad.jsx
+++ src/components/MaxPad.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function MaxPad({pos, name, id}) {
-  return (
-    <>
-        <div className={`pad absolute ${pos} flex justify-center items-center bg-black-one rounded-full w-28 h-28`}>
-          <div className={`flex justify-center items-center w-[90%] h-[90%] bg-white rounded-full`}>
-            <div id={name} className={`drum-pad flex justify-center items-center w-[70%] h-[70%] bg-black-one rounded-full text-white text-3xl font-bold`}>
-              {id}
-              <audio id={id} src="" className={`clip`}></audio>
+    const [active, setActive] = useState(false);
+
+    const handleClick = (event) => {
+        setActive(true);
+        setTimeout(() => setActive(false), 500);
+    };
+
+    return (
+        <>
+            <div onClick={(ev) => handleClick(ev)} className={`pad absolute ${pos} flex justify-center items-center bg-black-one rounded-full w-28 h-28`}>
+                <div className={`flex justify-center items-center w-[90%] h-[90%] ${active ? 'bg-yellow': 'bg-white'} rounded-full`}>
+                    <div id={name} className={`drum-pad flex justify-center items-center w-[70%] h-[70%] bg-black-one rounded-full ${active ? 'text-yellow': 'text-white'} text-3xl font-bold`}>
+                        {id}
+                        <audio id={id} src="" className={`clip`}></audio>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-    </>
-  )
+        </>
+    );
 }
 
 export default MaxPad;
